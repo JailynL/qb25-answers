2db3fb1012be6fa27a19e582d56e7a7db4a645fe
Jailyn Loor <Jloor1@jh.ed>
2025-10-25 12:59:43 -0400

    attempt 2

---
+++ week5/Readme.md
@@ -0,0 +1,9 @@
+Step 2.2:
+Question 1: The slope for this linear model is 0.38, a positive yet slow increase. In plain language, this
+means that maternal DNMs increase roughly ~0.38 every year of materanl age. Therefore, it does match out plot, as visually, we see a slight increase trend in the data. 
+Question 2: Yes, it is significant. The p value is <2e-16 ***, which is very low. This means that the observations we are seeing most likely did not happen by chance, therefore, maternal age tends is a good predictor of maternal DNMs. 
+
+Step 3.3 
+Question 1: The slope for this linear model is 1.35, a postive yet steeper slope. This means that paternal DNMs increase roughly ~1.35 units every year of paternal age. Therefore, it does match our plot as our plot has a much faster postive slope (when compared to the mother plot)
+Question 2: Yes, it is significant. The p value is <2e-16 ***, which is very low. This means that the observations we are seeing most likely did not happen by chance, therefore, paternal age is a good predictor of paternal DNMs
+
--- week5/Week5.R
+++ week5/Week5.R
@@ -21,7 +21,7 @@ momplot <- ggplot(merged_data, aes(x= mother, y= Mother_age)) + geom_point (colo
                 x = "count of maternal DNMs",
                 y = "count of maternal age")
 momplot
-ggsave("/Users/cmdb/qb25-answers/week5/2.1MaternalGraph.png", plot =momplot,  width = 10, height = 15)
+ggsave("/Users/cmdb/qb25-answers/week5/ex2_a.png", plot =momplot,  width = 10, height = 15)
 
 
 #Paternal Plot
@@ -30,7 +30,7 @@ dadplot <- ggplot(merged_data, aes(x= father, y= Father_age)) + geom_point (colo
   x = "count of paternal DNMs",
   y = "count of paternal age")
 dadplot
-ggsave("/Users/cmdb/qb25-answers/week5/2.1PaternalGraph.png",plot =dadplot, width = 10, height = 15)
+ggsave("/Users/cmdb/qb25-answers/week5/ex2_b.png",plot =dadplot, width = 10, height = 15)
 
 #Step 2.2 OLS- maternal age vs maternal DNMs
 linearmodel <- lm(merged_data$mother ~ merged_data$Mother_age, data = merged_data) #first is the response variable vs predictor varaible)
@@ -39,4 +39,18 @@ summary(linearmodel)
 #Step 2.3 OLS- paternal age vs paternal DNMs
 Paternallinearmodel <- lm(merged_data$father ~ merged_data$Father_age, data = merged_data) #first is the response variable vs predictor varaible)
 summary(Paternallinearmodel)
+
+#Step 2.4 Making predication 
+newpaternalpredication <- tibble(Father_age = 50.5)
+predict(Paternallinearmodel, newdata = newpaternalpredication)
+
+#step 2.5 Compare Distributions on maternal vs parental DNMs
+plot2 <- merged_data %>% pivot_longer(c(mother,father), names_to = "Parent_Type", values_to = "dnm_count") %>%
+  ggplot(aes(x= dnm_count, fill = Parent_Type)) +
+  geom_histogram(alpha = 0.3, position = "identity") + scale_fill_manual(values =c("father" = "blue", "mother" = "pink")) +labs(
+    title = "Maternal Versus Paternal DNMs",
+    x = "Number of DNMs",
+    y = "Count of Probands")
+plot2
+ggsave("/Users/cmdb/qb25-answers/week5/ex2_c.png", plot= plot2, width = 10, height = 15)
 
